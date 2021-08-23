bdf3e677047cd4960a3d736dd652be9613be981b
sziegler <sziegler@hcnetwork.org>
2021-08-23 15:46:06 -0400

    Sync

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,2 +1,27 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View } from "react-native";
+
+const ComponentsScreen = () => {
+  const greeting = <Text>Hello as a variable..</Text>;
+  const myName = "Scott";
+  return (
+    <View>
+      <Text style={styles.textStyle}>React Native... wee!</Text>
+      {greeting}
+      <Text style={styles.subHeaderStyle}>{myName} </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 25,
+    textAlign: "center",
+    marginTop: 10,
+  },
+  subHeaderStyle: {
+    fontSize: 50,
+  },
+});
+
+export default ComponentsScreen;
