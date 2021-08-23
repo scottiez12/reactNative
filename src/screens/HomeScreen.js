1608554140fb15c896e283ca6ec59e4f5c077b98
sziegler <sziegler@hcnetwork.org>
2021-08-23 16:51:38 -0400

    Props and Nav

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,42 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, Button, TouchableOpacity, View } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
+//destructuring on this component function... but could be props instead of {navigation}
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text style={styles.text}>HomeScreen</Text>
+      <Button
+        style={styles.margin}
+        title="Tap Here to go to the Components Screen"
+        onPress={() => {
+          navigation.navigate("Components");
+        }}
+      />
+      <Button
+        title="Go To List Demo"
+        onPress={() => navigation.navigate("List")}
+      />
+      {/* <TouchableOpacity
+        style={styles.margin}
+        onPress={() => {
+          props.navigation.navigate("List");
+        }}
+      >
+        <Text>Go To List Screen</Text>
+        <Text>Go To List Screen</Text>
+      </TouchableOpacity> */}
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   text: {
-    fontSize: 30
-  }
+    fontSize: 30,
+  },
+  margin: {
+    marginTop: 20,
+  },
 });
 
 export default HomeScreen;
--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,10 +1,44 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 
 const ListScreen = () => {
-  return <Text>List Screen.</Text>;
+  const friends = [
+    { name: "Something0", age: 10 },
+    { name: "Something1", age: 12 },
+    { name: "Something2", age: 21 },
+    { name: "Something3", age: 14 },
+    { name: "Something4", age: 16 },
+    { name: "Something5", age: 61 },
+    { name: "Something6", age: 51 },
+    { name: "Something7", age: 41 },
+    { name: "Something8", age: 22 },
+  ];
+
+  return (
+    <FlatList
+      horizontal={false}
+      showsHorizontalScrollIndicator={false}
+      keyExtractor={(friend) => {
+        friend.name;
+      }}
+      data={friends}
+      renderItem={({ item }) => {
+        return (
+          <Text style={styles.textStyle}>
+            {item.name} -- Age {item.age}
+          </Text>
+        );
+      }}
+    />
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  listStyles: {},
+  textStyle: {
+    marginVertical: 50,
+    marginLeft: 10,
+  },
+});
 
 export default ListScreen;
