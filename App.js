ffa21365bf2c8f3eb36eef80b047d10dbbafd0b6
sziegler <sziegler@hcnetwork.org>
2021-08-31 16:50:12 -0400

    Sync

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import ListScreen from "./src/screens/ListScreen";
 import ImageScreen from "./src/screens/ImageScreen";
 import CounterScreen from "./src/screens/CounterScreen";
 import ColorScreen from "./src/screens/ColorScreen";
+import SquareScreen from "./src/screens/SquareScreen";
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
@@ -14,6 +15,7 @@ const navigator = createStackNavigator(
     Image: ImageScreen,
     Counter: CounterScreen,
     Colors: ColorScreen,
+    Square: SquareScreen,
   },
   {
     initialRouteName: "Home",
